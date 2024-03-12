46470ee1f01b04baea71cb51b4d506ee4cedd5f6
Oskar Manhart <52569953+oskardotglobal@users.noreply.github.com>
2024-03-12 20:53:01 +0100

    feat: hints

--- scripts/functions.js
+++ scripts/functions.js
@@ -1,26 +1,3 @@
-function gibHinweis(sicherheitsstufe) {
-    switch (sicherheitsstufe) {
-        case 1:
-            alert(transform("TWFuY2htYWwgYmVmaW5kZW4gc2ljaCB3aWNodGlnZSBJbmZvcm1hdGlvbmVuIGltIFF1ZWxsY29kZSBlaW5lciBXZWJzaXRlLg=="));
-            break;
-        case 2:
-            alert(transform("TWFuY2htYWwga2FubiBtYW4gZWluZW4gUGFzc3dvcnRzY2h1dHogYXVjaCBlaW5mYWNoIHVtZ2VoZW4sIGluZGVtIG1hbiBkaWUgU3RydWt0dXIgZGVyIFVSTCBhbmF5bHNpZXJ0Lg=="));
-            break;
-        case 3:
-            alert(transform("Q29tcHV0ZXIgc3BlaWNoZXJuIFplaWNoZW4gaW4gRm9ybSB2b24gQmluYWVyY29kZXMsIGRpZSBudXIgYXVzIEVpbnNlbiB1bmQgTnVsbGVuIGJlc3RlaGVuLiBVbSBkaWVzZSBTZXF1ZW56ZW4gd2llZGVyIGluIEJ1Y2hzdGFiZW4gdW13YW5kZWxuIHp1IGtvZW5uZW4sIGRpZSBkZXIgTWVuc2NoIGxlc2VuIGthbm4sIGJlbm9ldGlndCBtYW4gVGFiZWxsZW4sIGluIGRlbmVuIGplZGVtIFNjaHJpZnR6ZWljaGVuIGVpbmUgWmlmZmVyIHp1Z2VvcmRuZXQgd2lyZC4gWnVkZW0gbXVzcyBtYW4gaW4gZGVyIExhZ2Ugc2VpbiwgQmluYWVyemFobGVuIGluZCBEZXppbWFsemFobGVuIHVtenV3YW5kZWxuLg=="));
-            break;
-        case 4:
-            alert(transform("RWluZW4gemllbWxpY2ggZ3V0ZW4gU2NodXR6IHZvciB1bmJlZnVndGVuIFp1Z3JpZmZlbiBiaWV0ZW4gdmVyc2NoaWVkZW5lIE1ldGhvZGVuIGRlciBWZXJzY2hsdWVzc2VsdW5nIChLcnlwdG9ncmFmaWUpLiBTb2xsdGUgbWFuIGF1ZiB2ZXJzY2hsdWVzc2VsdGUgSW5mb3JtYXRpb25lbiBzdG9zc2VuLCBtdXNzIG1hbiB6dW5hZWNoc3QgdmVyc3VjaGVuIGhlcmF1c3p1ZmluZGVuLCBtaXQgd2VsY2hlbSBWZXJmYWhyZW4gdmVyc2NobHVlc3NlbHQgd3VyZGUgKHouQi4gVHJhbnNwb3NpdGlvbiBvZGVyIFN1YnN0aXR1dGlvbikgdW5kIHdlbGNoZXIgU2NobHVlc3NlbCBnZWdlYmVuZW5mYWxscyBiZW51dHp0IHd1cmRlLg=="));
-            break;
-        case 7:
-            alert(transform("QXVjaCBpbiBKYXZhc2NyaXB0IGdpYnQgZXMgVmFyaWFibGVuIG1pdCBkZXJlbiBXZXJ0ZW4gbWFuIHJlY2huZW4ga2Fubiwgd2VubiBlcyBzaWNoIHVtIFphaGxlbiBoYW5kZWx0LiBVbSBSZWNobnVuZ2VuIHdpZWRlcmhvbHQgZHVyY2hmdWVocmVuIHp1IGtvZW5uZW4sIGJlbnV0enQgbWFuIHp1bSBCZWlzcGllbCBmb3ItU2NobGVpZmVuLiBVbSBkZXJlbiBGdW5rdGlvbiBuYWNodm9sbHppZWhlbiB6dSBrb2VubmVuLCBiaWV0ZXQgc2ljaCBlaW5lIEludGVybmV0cmVjaGVyY2hlIGFuLg=="));
-            break;
-        case 8:
-            alert(transform("SW4gVmFyaWFibGVuIGthbm4gbWFuIG5pY2h0IG51ciBaYWhsZW4sIHNvbmRlcm4gYXVjaCBCdWNoc3RhYmVuLCBXb3J0ZSB1bmQgU2FldHplIHNwZWljaGVybiB1bmQgbWFuaXB1bGllcmVuLiBNaXR0ZWxzIGRlciBNZXRob2RlbiB2b24gU3RyaW5nIGtvZW5uZW4gdmllbGUgdmVyc2NoaWVkZW5lIFZlcmFlbmRlcnVuZ2VuIHZvcmdlbm9tbWVuIHdlcmRlbi4gKGh0dHBzOi8vd3d3Lnczc2Nob29scy5jb20vanMvanNfc3RyaW5nX21ldGhvZHMuYXNwKQ=="));
-            break;
-    }
-}
-
 function decodePF(zeichenkette) {
     if (zeichenkette === "WlemcikrihlShcad")
         return transform("V2lsaGVsbVNjaGlja2FyZA==");
@@ -59,5 +36,5 @@ function binCheck(zeichenkette) {
 }
 
 function transform(zeichenkette) {
-    return atob(zeichenkette); /* B64  decode atob(); encode btoa();*/
+    return atob(zeichenkette);
 }
--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,9 +1,22 @@
 export function Footer({number}: { number: number }) {
+    const hints = {
+        1: "Manchmal befinden sich wichtige Informationen im Quellcode einer Website.",
+        2: "Computer speichern Zeichen in Form von Binärcodes, die nur aus Einsen und Nullen bestehen. Um diese Sequenzen wieder in Buchstaben umwandeln zu können, die der Mensch lesen kann, benötigt man Tabellen, in denen jedem Schriftzeichen eine Ziffer zugeordnet wird. Zudem muss man in der Lage sein, Binärzahlen ind Dezimalzahlen umzuwandeln.",
+        3: "Einen ziemlich guten Schutz vor unbefugten Zugriffen bieten verschiedene Methoden der Verschlüsselung (Kryptografie). Sollte man auf verschlüsselte Informationen stossen, muss man zunächst versuchen herauszufinden, mit welchem Verfahren verschlüsselt wurde (z.B. Transposition oder Substitution) und welcher Schlüssel gegebenenfalls benutzt wurde.",
+        6: "Auch in Javascript gibt es Variablen mit deren Werten man rechnen kann, wenn es sich um Zahlen handelt. Um Rechnungen wiederholt durchführen zu können, benutzt man zum Beispiel for-Schleifen. Um deren Funktion nachvollziehen zu können, bietet sich eine Internetrecherche an.",
+        7: "In Variablen kann man nicht nur Zahlen, sondern auch Buchstaben, Worte und Sätze speichern und manipulieren. Mittels der Methoden von String können viele verschiedene Veränderungen vorgenommen werden. (https://www.w3schools.com/js/js_string_methods.asp)"
+    };
+
+    // @ts-ignore
+    const hint = hints[number];
+
     return (
         <>
             <hr/>
             <footer role="contentinfo">
-                <button id={"hint"}>Hinweis</button>
+                <button id={"hintbtn"}>Hinweis</button>
+                <div style={{display: "none"}} id={"hint"}>{hint}</div>
+
                 <br/>
                 <small>
                     Hacker's Night am Otto-Nagel-Gymnasium {new Date().getFullYear()}
