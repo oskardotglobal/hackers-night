58d03e98284617a83756402f51d117954da2325c
Oskar Manhart <52569953+oskardotglobal@users.noreply.github.com>
2024-03-14 10:03:18 +0100

    fix: show error on question1 if password wrong

--- static/js/index.js
+++ static/js/index.js
@@ -3,6 +3,8 @@
         if (document.getElementById("idBenutzername").value === "Holger Henzler-Hübner" && document.getElementById("idPasswort").value === "902995031") {
             alert("Eingabe korrekt.");
             n("Holger Henzler-Hübner", "902995031");
+        } else {
+            alert("Passwort oder Benutzername falsch!");
         }
     }
 
